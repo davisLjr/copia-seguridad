a85998817c4cf2858cd044de9ec9b452e6252092
davisLjr <davisprojectjr@gmail.com>
2024-01-23 11:46:40 -0300

    fix: correccion de textos

--- components/Home/About/index.tsx
+++ components/Home/About/index.tsx
@@ -6,19 +6,19 @@ export const AboutSection = () => {
     <div className="about-panels">
       <Panels
         title="Copiar el HTML"
-        description="El primer paso sería copiar el HTML que necesites, este puede ser proporcionado por un desarollador, o puedes conseguirlo en Storybook."
+        description="El primer paso sería copiar el HTML que necesites; este puede ser proporcionado por un desarrollador, o puedes conseguirlo en Storybook."
         imageSrc="/assets/about/step-1.png"
       />
 
       <Panels
         title="Colocar el HTML"
-        description="El segundo paso sería colocar el HTML previamente copiado, y usarlo en el editor online de esta pagina."
+        description="El segundo paso sería colocar el HTML previamente copiado y usarlo en el editor en línea de esta página."
         imageSrc="/assets/about/step-2.png"
       />
 
       <Panels
         title="Editar el HTML"
-        description="Este seria el tercer y ultimo paso, a esta altura podras editar el HTML dependiendo de tus requerimientos y verificar los resultados visuales en tiempo real, garantizando que el codigo que usaras se vea de la manera correcta en todo momento. "
+        description="Este sería el tercer y último paso. A esta altura, podrás editar el HTML según tus requerimientos y verificar los resultados visuales en tiempo real, garantizando que el código que usarás se vea de la manera correcta en todo momento."
         imageSrc="/assets/about/step-3.png"
       />
     </div>
